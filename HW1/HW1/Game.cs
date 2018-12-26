cd4c57edd995296b78c01b604907df39e7083041
bunny12rabbit <bunny12rabbit@gmail.com>
2018-12-26 18:43:12 +0300

    Finnaly, damn!
    Ура блин! Методичка кривущая! Потратил весь день чтобы написать код, который должен работать как задумывалось в методичке.....

---
+++ HW1/HW1/Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace HW1
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        public static void  Init (Form form)
+        {
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick (object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            foreach(BaseObject obj in _objs)
+            {
+                obj.Draw();
+            }
+            Buffer.Render();
+
+        }
+
+        public static void Update ()
+        {
+            foreach(BaseObject obj in _objs)
+            {
+                obj.Update();
+            }
+        }
+
+        public static BaseObject[] _objs;
+
+        public static void Load ()
+        {
+            _objs = new BaseObject[60];
+            for (int i = 0; i <= _objs.Length/10; i++)
+            {
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(10-i*4, 10-i*4), new Size(50, 50));
+            }
+            for (int i = _objs.Length/10; i < _objs.Length; i++)
+            {
+                _objs[i] = new Star(new Point(600, i * 15), new Point(10 -i, 10 -i), new Size(5, 5));
+            }
+        }
+    }
+}
--- HW1/HW1/Program.cs
+++ HW1/HW1/Program.cs
@@ -16,7 +16,11 @@ namespace HW1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            Application.Run(form);
         }
     }
 }
